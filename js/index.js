32cf566460bfb116e125036d6daa4fed83633ef5
Jiamin Lim <jiaminlim93@gmail.com>
2018-10-10 12:45:46 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+
+document.addEventListener('keypress', (event) => {
+  const keyName = event.key;
+  if(keyName ==='j'){
+    return event.preventDefault()
+  } else {
+    alert('keypress event\n\n' + 'key: ' + keyName);
+  }
+});
